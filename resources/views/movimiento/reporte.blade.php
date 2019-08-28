31c208806c980ca15fb491f5a05f873b68cf08ee
Alejandra <alejandra.tolaba02@gmail.com>
2019-08-28 10:48:35 -0300

    Reporte de movimiento de caja terminado

--- resources/views/movimiento/reporte.blade.php
+++ resources/views/movimiento/reporte.blade.php
@@ -5,16 +5,18 @@
     <title>Reporte de Movimientos</title>
     <style>
         h1{
-            border-top: 1px solid  #5D6975;
-            border-bottom: 1px solid  #5D6975;
-            color: #5D6975;
+            /* border-top: 1px solid  #5D6975;
+            border-bottom: 1px solid  #5D6975;*/
+            color: #5D6975; 
             font-size: 2.4em;
             line-height: 1.4em;
             font-weight: normal;
             text-align: center;
-            margin: 0 0 20px 0;
+            vertical-align: middle;
+            margin: 20px 0 0 0;
+
         }
-        h2,h3{
+        h2,h3,h4{
             color: #5D6975;
             font-weight: normal;
             text-align: left;
@@ -35,30 +37,58 @@
             text-align: center;
             white-space: nowrap;        
             font-weight: normal;
+            border: 1px solid #C1CED9;
+            border-left: none;
+            border-right: none;
+            padding: 6px 5px;
+            color: #5D6975;
         }
         table th {
-            padding: 5px 20px;
+            padding: 6px 5px;
+            border: 1px solid #C1CED9;
             color: #5D6975;
-            border-bottom: 1px solid #C1CED9;
             white-space: nowrap;        
             font-weight: normal;
+            border-left: none;
+            border-right: none;
         }
-        .table-bordered th{
+        .table-bordered {
             color: black;
+            border: 1px solid #C1CED9;
         }
         .movements{
-            margin-left: 210px;
+            width: 100%;
+            color: #5D6975;
+            border: 1px solid #C1CED9;
         }
+        .img-redonda {
+            width:100px;
+            height:100px;
+            border-radius: 50%;
+            vertical-align: middle;
+            margin: 0px 0px 20px 0px;
+            float: left;
+        }
+        /* #imagen{
+            width:100px;
+            height:100px;
+            border-radius: 50%;
+            background-image:url("./imagenes/pf/logo1.jpg");
+            float: left;
+        } */
     </style>
   </head>
   <body >
     <header class="clearfix">
-      <h1>Reporte de Movimientos</h1>
+        <!--<div id="imagen"></div><img class="img-circle" src="./imagenes/pf/logo1.jpg" id="img" alt="Logo"> <img src="{{asset('/imagenes/pf/logo1.jpg')}}"  class="img-circle" alt="Logo" height="30px" width="30px"> -->
+        <!-- <div aling="center"><img class="img-redonda" src="./imagenes/pf/logo1.jpg" id="img" alt="Logo"></div> -->
+        <img class="img-redonda" src="./imagenes/pf/logo1.jpg" id="img" alt="Logo">
+        <h1> Reporte de Movimientos </h1>
     </header>
             @if($desde == $hasta)
-                <h2 name="titulo" class="box-tittle">Movimientos del {{date("d-m-Y",strtotime($desde))}}</h2>
+                <h3 name="titulo" class="box-tittle">Movimientos del {{date("d-m-Y",strtotime($desde))}}</h3>
             @else
-                <h2 name="titulo" class="box-tittle">Movimientos desde {{date("d-m-Y",strtotime($desde))}} hasta {{date("d-m-Y",strtotime($hasta))}}</h2>
+                <h3 name="titulo" class="box-tittle">Movimientos desde el {{date("d-m-Y",strtotime($desde))}} hasta el {{date("d-m-Y",strtotime($hasta))}}</h3>
             @endif
             <main>
                 <table style="border-bottom:2px;">
@@ -83,20 +113,38 @@
                         @endforeach
                     </tbody>
                 </table> 
-                <div class="col-xs-6 movements">
-                    <table class="table table-bordered" style="width: 150px">
-                        <tr style="background-color: #F2F4F4;">
-                            <th  class="text-center">TOTAL INGRESOS</th>
-                            <th  class="text-center">TOTAL EGRESOS</th>
-                            <th  class="text-center">TOTAL CAJA</th>
+                <br>
+                <div class="col-xs-6 ">
+                    <table class="table table-bordered" >
+                        <tr style="background-color: #F2F4F4; border: 1px solid #C1CED9;">
+                            <th  class="text-center" style="color: #5D6975;"><b> INGRESOS EN CONTADO</b></th>
+                            <th  class="text-center" style="color: #5D6975;"><b> EGRESOS EN CONTADO </b></th>
                         </tr>
                         <tr>
                             <td  class="text-center">${{$totalIngreso->totalIngreso}}</td>
                             <td  class="text-center">${{$totalEgreso->totalEgreso}}</td>
-                            <td  class="text-center">${{$total}}</td>   
                         </tr>
                     </table>
-    </div>
+                </div>
+                <br>
+                <div class="col-xs-6">
+                    <table class="table table-bordered">
+                        <tr style="background-color: #F2F4F4;">
+                            <th  class="text-center" style="color: #5D6975;"><b>TOTAL CONTADO</b></th>
+                            <th  class="text-center" style="color: #5D6975;"><b>TOTAL CRÉDITO</b></th>
+                            <th  class="text-center" style="color: #5D6975;"><b>TOTAL DÉBITO</b></th>
+                            <th  class="text-center" style="color: #5D6975;"><b>TOTAL DÉBITO AUTOMÁTICO</b></th>
+                            <th  class="text-center" style="color: #5D6975;"><b>TOTAL</b></th>
+                        </tr>
+                        <tr>
+                            <td  class="text-center">${{$totales->totalContado}}</td>
+                            <td  class="text-center">${{$totales->totalCredito}}</td>
+                            <td  class="text-center">${{$totales->totalDebito}}</td>
+                            <td  class="text-center">${{$totales->totalDebitoAutomatico}}</td>
+                            <td  class="text-center" style="background-color: #F2F4F4;">${{$total->total}}</td>
+                        </tr>
+                    </table>
+                </div>
             </main>
     <footer>
     </footer>  
